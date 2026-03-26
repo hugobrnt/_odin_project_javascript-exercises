f3dc63873d61383c21a875e044bfeeff2f92f9e6
hugobrnt <go.brunet@gmail.com>
2026-03-26 12:08:18 +0100

    Completed copyAndSortArray function with strings

--- array_methods_ex/copy_and_sort_array/script.js
+++ array_methods_ex/copy_and_sort_array/script.js
@@ -1,11 +1,12 @@
 function	copyAndSortArray(arr)
 {
-	return (arr.toSorted((a, b) => a - b));
+	return (arr.slice().sort());
 }
 
 function	main()
 {
-	const	arr = [3, 6, 2, 7, 345, -1234, 576, 246, 0];
+	//const	arr = [3, 6, 2, 7, 345, -1234, 576, 246, 0];
+	const	arr = ["AA", "ZZ", "BB", "AAA", "CC"];
 	const	sortedCopy = copyAndSortArray(arr);
 	console.log(arr);
 	console.log(sortedCopy);
