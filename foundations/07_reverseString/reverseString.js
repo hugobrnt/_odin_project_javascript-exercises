d9239d6fbc9cfa776537b771d8459a3f7d5156b5
hugobrnt <go.brunet@gmail.com>
2026-03-26 14:37:12 +0100

    Finished reversed string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,34 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const	strArr = string.split("");
+	const	strLen = strArr.length;
+	let		i = 0;
+	let		j = strLen - 1;
+	let		temp;
 
+	console.log(strArr);
+	//debugger;
+	while (i < strLen / 2)
+	{
+		temp = strArr[i];
+		strArr[i] = strArr[j];
+		strArr[j] = temp;
+		j--;
+		i++;
+	}
+	console.log(strArr);
+	return (strArr.join(""));
 };
+
+function	main()
+{
+	const	str = "Coucou mec";
+
+	console.log(str);
+	console.log(reverseString(str));
+	return (0);
+}
+
+main();
 
 // Do not edit below this line
 module.exports = reverseString;
