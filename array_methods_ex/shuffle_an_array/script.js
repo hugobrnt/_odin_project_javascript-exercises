805a17d52d4581a675b2597d7c18edf51d1e9b9d
hugobrnt <go.brunet@gmail.com>
2026-03-26 12:51:34 +0100

    Modified the suffleArray function with Fisher-Yates algo
    In order to truly get equal probabilities for each permutation.

--- array_methods_ex/shuffle_an_array/script.js
+++ array_methods_ex/shuffle_an_array/script.js
@@ -1,37 +1,69 @@
 function	getRandomInteger(min, max)
 {
-	min = Math.ceil(min);
-	max = Math.floor(max);
+	//min = Math.ceil(min);
+	//max = Math.floor(max);
 
 	return (Math.round(Math.random() * (max - min) + min));
 }
 
 function	shuffleArray(arr)
 {
+	// Fisher-Yates Shuffle Algorithm
 	const	arrLength = arr.length;
-	const	pickedIndex = getRandomInteger(0, arrLength - 1);
+	let		randomIndex;
 	let		temp;
+	let		i;
 
 	if (arrLength === 0 || arrLength === 1)
 		return ;
-	temp = arr[0];
-	arr[0] = arr[pickedIndex];
-	arr[pickedIndex] = temp;
+	i = arrLength - 1;
+	while (i >= 0)
+	{
+		randomIndex = getRandomInteger(0, arrLength - 1);
+		temp = arr[randomIndex];
+		arr[randomIndex] = arr[i];
+		arr[i] = temp;
+		i--;
+	}
+}
+
+function	testShuffle()
+{
+	const	round = 1000000;
+	let 	count = {
+	'123': 0,
+	'132': 0,
+	'213': 0,
+	'231': 0,
+	'321': 0,
+	'312': 0
+	};
+
+	for (let i = 0; i < round; i++) {
+	let array = [1, 2, 3];
+	shuffleArray(array);
+	count[array.join('')]++;
+	}
+
+	// show counts of all possible permutations
+	for (let key in count) {
+		console.log(`${key}: ${Math.trunc(count[key] / round * 100)}%`);
+	}
 }
 
 function	main()
 {
-	const	arr = [2, 5, 9, 87];
+	// const	arr = [1, 2, 3];
 	
-	shuffleArray(arr);
-	console.log(arr);
+	// shuffleArray(arr);
+	// console.log(arr);
 
-	shuffleArray(arr);
-	console.log(arr);
+	// shuffleArray(arr);
+	// console.log(arr);
 
-	shuffleArray(arr);
-	console.log(arr);
-	
+	// shuffleArray(arr);
+	// console.log(arr);
+	testShuffle();
 	return (0);
 }
 
