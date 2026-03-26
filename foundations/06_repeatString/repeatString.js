7fca5cfd6814bed30f87bfaba89ca4d9f205b23e
hugobrnt <go.brunet@gmail.com>
2026-03-26 14:17:30 +0100

    Finished repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	let	result;
 
+	if (num < 0)
+		return ("Error");
+	result = "";
+	while (num > 0)
+	{
+		result += string;
+		num--;
+	}
+	return (result);
 };
 
+console.log(repeatString("Coucou", 5));
 // Do not edit below this line
 module.exports = repeatString;
