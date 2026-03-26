fe1cd8a7e2a28b48856efc7c5d0ab6db59d6d64f
hugobrnt <go.brunet@gmail.com>
2026-03-26 15:05:02 +0100

    Finished removeFromArray function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+	const	result = [];
+	let		i;
+
+	if (arr === undefined)
+		return ;
+	if (args === undefined)
+		return (arr);
+
+	arr.forEach((item) => {
+		if (!args.includes(item))
+			result.push(item);
+	});
+	return (result);
 };
 
+function	main()
+{
+	console.log(removeFromArray([1, 2, 3, 5, 44, 3], 3, 2));
+	return (0);
+}
+main();
 // Do not edit below this line
 module.exports = removeFromArray;
