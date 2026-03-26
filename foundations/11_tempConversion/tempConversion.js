8aa356ef06b4e4bbc6c630816ac29d20cb7dfafa
hugobrnt <go.brunet@gmail.com>
2026-03-26 16:03:41 +0100

    Completed tempConversion functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,38 @@
-const convertToCelsius = function() {
+const	roundTo = function(num, precision){
+	const	floatNumber = Number.parseFloat(num);;
+	const	factor = Math.pow(10, precision);
+
+	if (Number.isNaN(floatNumber)
+		|| !Number.isInteger(precision))
+		return ("Error");
+	return (Math.round(floatNumber * factor) / factor);
 };
 
-const convertToFahrenheit = function() {
+const	convertToCelsius = function(tempInFahrenheit) {
+	if (!Number.isInteger(tempInFahrenheit)
+		&& Number.isNaN(Number.parseFloat(tempInFahrenheit)))
+		return ("Error");
+	return (roundTo(tempInFahrenheit -= 32, 1));
 };
 
+const	convertToFahrenheit = function(tempInCelsius) {
+	if (!Number.isInteger(tempInCelsius)
+		&& Number.isNaN(Number.parseFloat(tempInCelsius)))
+		return ("Error");
+	return (roundTo(tempInCelsius += 32, 1));
+};
+
+function	main()
+{
+	const	celsius = parseFloat(prompt("Celsius temp:"));
+	const	fahrenheit = parseFloat(prompt("Fahrenheit temp: "));
+
+	console.log(`${celsius} celsius is ${convertToCelsius(celsius)} in fahrenheit`);
+	console.log(`${fahrenheit} fahrenheit is ${convertToFahrenheit(fahrenheit)} in celsius`);
+	return (0);
+}
+
+main();
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
