fd13660b2d625643a77eac53f0787ca974ada950
hugobrnt <go.brunet@gmail.com>
2026-04-08 15:12:40 +0200

    Finished palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (s) {
+	let	i;
 
+	i = 0;
+	while (s[i] === s[s.length - i - 1] && i < (s.length - 1) / 2)
+		i++;
+	console.log(i);
+	if (i === (s.length - 1) / 2)
+		return (true);
+	return (false);
 };
 
+console.log(palindromes("racecar"));
+console.log(palindromes("kayak"));
+console.log(palindromes("kouik"));
+console.log(palindromes("coucou"));
+
+
 // Do not edit below this line
-module.exports = palindromes;
+// module.exports = palindromes;
