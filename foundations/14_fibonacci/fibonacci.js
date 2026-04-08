402511911f388e8f3e5ed984d1ccea4c53976212
hugobrnt <go.brunet@gmail.com>
2026-04-08 15:18:41 +0200

    Finished Fibonacci in js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+	let	result;
 
+	if (index < 0)
+		return ("error");
+	if (index === 0 || index == 1)
+		return (1);
+	result = fibonacci(index - 1) + fibonacci(index - 2);
+	return (result);
 };
 
-// Do not edit below this line
-module.exports = fibonacci;
+console.log(fibonacci(3));
+console.log(fibonacci(5));
+console.log(fibonacci(7));
+console.log(fibonacci(1));
+console.log(fibonacci(0));
+console.log(fibonacci(-3));
+console.log(fibonacci(10));
+
+
+
+
+
+// // Do not edit below this line
+// module.exports = fibonacci;
